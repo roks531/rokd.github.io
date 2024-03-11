eec57eb6e7d0f64099ecb9411627cf27d10c5333
rokd <rok.damjanic@gmail.com>
2024-03-11 20:35:39 +0100

    algolia try5

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ const config = {
         indexName: 'rokd',
   
         // Optional: see doc section below
-        contextualSearch: false,
+        contextualSearch: true,
   
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         // externalUrlRegex: 'external\\.com|domain\\.com',
@@ -100,7 +100,7 @@ const config = {
         // },
   
         // // Optional: Algolia search parameters
-        // searchParameters: {},
+        searchParameters: {},
   
         // // Optional: path for search page that enabled by default (`false` to disable it)
         searchPagePath: 'search',
